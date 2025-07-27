239d3547a1a3e31abb3e0f577224c16b1e928717
Tom Pollak <tompollak1000@gmail.com>
2025-07-27 15:56:43 +0100

    x

--- train_gpt.py
+++ train_gpt.py
@@ -672,7 +672,7 @@ master_process = (rank == 0) # this process will do logging, checkpointing etc.
 config_container = [None]
 if master_process:
     wandb.init(project="modded-nanogpt")
-    config_container[0] = wandb.config
+    config_container[0] = dict(wandb.config)
 
 dist.broadcast_object_list(config_container, src=0)
 dist.barrier()
